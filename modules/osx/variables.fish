e06015ed5189449bfd7225fb0f97dc88348b9867
TJ Potenza <thomas@potenza.me>
2016-05-11 00:41:45 -0400

    Remove docker-machine vm support for startup performance reasons.

--- modules/osx/variables.fish
+++ modules/osx/variables.fish
@@ -4,6 +4,3 @@
 # host (Because it's synced online and consequently can
 # be accessed by a number of different devices)
 set -gx VAGRANT_DOTFILE_PATH ".vagrant-mbp"
-
-# Configure docker globals so docker-machine's vm is connected
-eval (docker-machine env)
